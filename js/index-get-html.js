8eaf0d48c36999b00c6b7d2d994a3a31eee3abb3
katrin <andreevak53@gmail.com>
2018-07-08 13:27:07 +0000

    initial

---
+++ js/index-get-html.js
@@ -0,0 +1,12 @@
+document.querySelector('.get-html')
+addEventListener('click', getHtml);
+
+function getHtml() {
+    const xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState === 4 && xhr.status === 200) {
+            document.querySelector('.container-html')
+            .innerHTML = xhr.responseText;
+        }
+    }
+}
